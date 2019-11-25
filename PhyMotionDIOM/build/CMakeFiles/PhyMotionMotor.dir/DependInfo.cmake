a75728cd493f9bdb50ab55f755b7a53ba040be8a
TEX-2 pc <pshcyrill@mail.ru>
2019-11-25 17:31:32 +0300

    add PhyMotionDIOM device server

---
+++ PhyMotionDIOM/build/CMakeFiles/PhyMotionMotor.dir/DependInfo.cmake
@@ -0,0 +1,28 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_CXX
+  "/home/a/TEX-2/servers/PhyMotionDIOM/ClassFactory.cpp" "/home/a/TEX-2/servers/PhyMotionDIOM/build/CMakeFiles/PhyMotionMotor.dir/ClassFactory.cpp.o"
+  "/home/a/TEX-2/servers/PhyMotionDIOM/PhyMotionDIOM.cpp" "/home/a/TEX-2/servers/PhyMotionDIOM/build/CMakeFiles/PhyMotionMotor.dir/PhyMotionDIOM.cpp.o"
+  "/home/a/TEX-2/servers/PhyMotionDIOM/PhyMotionDIOMClass.cpp" "/home/a/TEX-2/servers/PhyMotionDIOM/build/CMakeFiles/PhyMotionMotor.dir/PhyMotionDIOMClass.cpp.o"
+  "/home/a/TEX-2/servers/PhyMotionDIOM/PhyMotionDIOMStateMachine.cpp" "/home/a/TEX-2/servers/PhyMotionDIOM/build/CMakeFiles/PhyMotionMotor.dir/PhyMotionDIOMStateMachine.cpp.o"
+  "/home/a/TEX-2/servers/lib/PhyMotionControlCMD.cpp" "/home/a/TEX-2/servers/PhyMotionDIOM/build/CMakeFiles/PhyMotionMotor.dir/home/a/TEX-2/servers/lib/PhyMotionControlCMD.cpp.o"
+  "/home/a/TEX-2/servers/PhyMotionDIOM/main.cpp" "/home/a/TEX-2/servers/PhyMotionDIOM/build/CMakeFiles/PhyMotionMotor.dir/main.cpp.o"
+  )
+set(CMAKE_CXX_COMPILER_ID "GNU")
+
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  "../"
+  "/usr/include/tango"
+  "../../lib"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
